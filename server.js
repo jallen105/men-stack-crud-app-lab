ce26326761dd5166361bea9039da26205ce0c92c
Jonathan <jonathanallen105@gmail.com>
2025-09-04 11:01:24 -0400

    added styling

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express()
 const mongoose = require("mongoose")
 const methodOverride = require("method-override")
 const morgan = require("morgan")
+const path = require("path")
 
 const port = process.env.PORT
 
